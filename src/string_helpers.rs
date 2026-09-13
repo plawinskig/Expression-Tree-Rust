1e057b7751684ca2e099dc27efe36ba977e75a3b
plawinskig <plawinskig@gmail.com>
2026-09-14 00:44:01 +0200

    Implement cut_white_beginning function and refactor crop_letters

--- src/string_helpers.rs
+++ src/string_helpers.rs
@@ -12,6 +12,24 @@ pub fn find_text(text: &str) -> Option<usize>
     return None;
 }
 
+fn crop_letters(s: &str, pos: usize) -> &str {
+    match s.char_indices().skip(pos).next() {
+        Some((pos, _)) => &s[pos..],
+        None => "",
+    }
+}
+
+pub fn cut_white_beginning(text: &str) -> &str
+{
+    let pos = find_text(text);
+
+    match pos {
+        Some(pos) if pos > 0 => crop_letters(text, pos),
+        None if !text.is_empty() => "",
+        _ => text
+    }
+}
+
 #[cfg(test)]
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
