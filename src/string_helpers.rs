516ce9f87f9a1d3afa4231255f6db6467c60fbe4
plawinskig <plawinskig@gmail.com>
2026-09-14 00:26:11 +0200

    Make find_text function public and add tests

--- src/string_helpers.rs
+++ src/string_helpers.rs
@@ -1,5 +1,5 @@
 
-fn find_text(text: &str) -> Option<usize>
+pub fn find_text(text: &str) -> Option<usize>
 {
     for (i, chr) in text.chars().enumerate()
     {
@@ -10,4 +10,17 @@ fn find_text(text: &str) -> Option<usize>
     }
 
     return None;
+}
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_find_text() {
+        assert_eq!(find_text("     56789"), Some(5));
+        assert_eq!(find_text("0123456789"), Some(0));
+        assert_eq!(find_text("          "), None);
+    }
 }
